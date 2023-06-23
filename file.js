b47f473b590dd1e121a8bf9a9dc73f420589fc4f
Jasmeet Kaur <jasmeet13112001@gmail.com>
2023-06-23 21:55:27 +0530

    Initial commit

---
+++ file.js
@@ -0,0 +1,28 @@
+function add(a, b, c) {
+    console.log("Value is:");
+    return a + b + c;
+}
+
+// let a = 1;
+// let b = 3;
+// let c = 2;
+
+// let d = add(a,b,c);
+// console.log(d);
+
+function maximum(a,b){
+    if (a>b){
+        console.log(a,"is greater");
+    }
+    else if(a==b){
+        console.log(a," and ",b,"are equal");
+    }
+    else{
+        console.log(b,"is greater");
+    }
+}
+
+let a= 237;
+let b=24;
+maximum(a,b);
+
