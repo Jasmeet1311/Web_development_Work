fa1ce92e69e776ed567eee2ce267c8cd1b2471f4
Jasmeet Kaur <jasmeet13112001@gmail.com>
2023-07-01 18:56:36 +0530

    serving HTML file usong NodeJs

---
+++ Nodejs04_creatingServer.js
@@ -0,0 +1,13 @@
+// Reading and displaying the html file using nodejs
+
+const http = require("http");
+const fs = require("fs");
+const fileContent = fs.readFileSync("css_08_navBar_displayProperty.html");
+
+const server = http.createServer((req,res)=>{
+    res.writeHead(200,{'Content-type':'text/html'});
+    res.end(fileContent);
+})
+
+server.listen(80,'127.0.0.1');
+console.log("Listening on port 80");
